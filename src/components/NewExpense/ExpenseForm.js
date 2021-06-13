49f72fc2f223220a8957909fc85aace1a51fc3ac
soniharshil54 <sharshil43@yahoo.com>
2021-06-13 15:49:29 +0530

    nr: two way binding ( reset form inputs programmaticaly )

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -46,6 +46,12 @@ const ExpenseForm = () => {
         }
 
         console.log(expenseData);
+
+        setUserInput({
+            enteredTitle: '',
+            enteredAmount: '',
+            enteredDate: '',
+        });
     };
 
     return (
@@ -53,15 +59,15 @@ const ExpenseForm = () => {
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' onChange={titleChangeHandler} />
+                    <input type='text' value={userInput.enteredTitle} onChange={titleChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler} />
+                    <input type='number' min="0.01" step="0.01" value={userInput.enteredAmount} onChange={amountChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+                    <input type='date' min="2019-01-01" max="2022-12-31" value={userInput.enteredDate} onChange={dateChangeHandler} />
                 </div>
             </div>
             <div className="new-expense__actions">
