5f5ad260682a8c9480f51aa550e1d207b3a9e168
soniharshil54 <sharshil43@yahoo.com>
2021-06-20 15:25:56 +0530

    cr: use map array to list expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,32 +6,13 @@ function Expenses(props) {
     const {items} = props;
     return (
         <Card className="expenses">
-            {/* {items.map((expense) => {
-                <ExpenseItem
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date} />
-            })} */}
-            <ExpenseItem
-                title={items[0].title}
-                amount={items[0].amount}
-                date={items[0].date}
-            />
-            <ExpenseItem
-                title={items[1].title}
-                amount={items[1].amount}
-                date={items[1].date}
-            />
-            <ExpenseItem
-                title={items[2].title}
-                amount={items[2].amount}
-                date={items[2].date}
-            />
-            <ExpenseItem
-                title={items[3].title}
-                amount={items[3].amount}
-                date={items[3].date}
-            />
+            {items.map((expense) =>  (
+                    <ExpenseItem
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date} 
+                    />
+            ))}
         </Card>
     )
 }
